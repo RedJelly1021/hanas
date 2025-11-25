59dc1cdcb058aff65bf42ba9455fbf3685794179
JELLY-DESKTOP\ekgus <gus1021@naver.com>
2025-11-26 07:09:51 +0900

    ✨ feat: 즐겨찾기 기능을 위한 FavoriteProvider 클래스 추가

---
+++ lib/providers/favorite_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class FavoriteProvider extends ChangeNotifier
+{
+  final List<String> _favorites = []; //즐겨찾기 아이템 리스트
+  List<String> get favorites => _favorites; //즐겨찾기 아이템 getter
+
+  bool isFavorite(String name) //이름이 즐겨찾기에 있는지 확인
+  {
+    return _favorites.contains(name);
+  }
+
+  void toggleFavorite(String name) //즐겨찾기 토글 메서드
+  {
+    if (_favorites.contains(name)) //이미 즐겨찾기에 있으면
+      _favorites.remove(name); //제거
+    else //즐겨찾기에 없으면
+      _favorites.add(name); //추가
+    notifyListeners(); //변경 사항 알림
+  }
+}
